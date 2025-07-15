1f7d8770a60642bf9be29a446e6749f732eabc3b
PilotGoPro <julianalexcohen@hotmail.com>
2025-07-15 12:23:42 -0400

    Update and rename Script.lua to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
 if game.PlaceId == 12240122896 then
 loadstring(game:HttpGet("https://pastefy.app/fbNVhln0/raw"))()
-if game.PlaceId == 2753915549 then
-loadstring(game:HttpGet("https://pastefy.app/t0a3qBNu/raw"))()
+if else
+print("sorry this game is not yet added to jchub");
