ee8276662c8b3203c9766992a05d19907a40cc7d
PilotGoPro <julianalexcohen@hotmail.com>
2025-07-15 12:31:37 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 if game.PlaceId == 17282136897 then
-loadstring(game:HttpGet("https://pastebin.com/raw/JJgy1LSa"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/Doors_sandbox.lua"))()
