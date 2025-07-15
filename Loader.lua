9675da0def9f8731c5c666a2dd945c6f25bac32c
PilotGoPro <julianalexcohen@hotmail.com>
2025-07-15 12:31:02 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,2 @@
 if game.PlaceId == 17282136897 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/Doors_sandbox.lua"))()
-if else
-print("sorry this game is not yet added to jchub");
+loadstring(game:HttpGet("https://pastebin.com/raw/JJgy1LSa"))()
