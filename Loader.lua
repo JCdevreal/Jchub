00a00cae13786ff09a03cec936a7ffecf7ace9aa
PilotGoPro <julianalexcohen@hotmail.com>
2025-07-16 16:15:27 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,10 @@
 if game.PlaceId == 17282136897 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/Doors_sandbox.lua"))()
+if game.PlaceId == 17729873503 then
+print("working");
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "JCHUB", -- Required
+	Text = "JCHUB does not support this game yet!", -- Required
+	Icon = "nil" -- Optional
+})
