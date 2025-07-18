2a0aa286f9539767099c541a7cb3abcf48e7a46a
PilotGoPro <julianalexcohen@hotmail.com>
2025-07-18 15:31:09 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
 if game.PlaceId == 17282136897 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/Doors_sandbox.lua"))()
-if game.PlaceId == 17729873503 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/DoorsRPGame.lua"))()
+if game.PlaceId == 110258689672367 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/Doors.lua"))()
