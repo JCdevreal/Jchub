4307e62fbe7ab068c1afd65e308788dd5e54f1a5
PilotGoPro <julianalexcohen@hotmail.com>
2025-07-22 13:26:13 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,4 @@
 if game.PlaceId == 17282136897 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/Doors_sandbox.lua"))()
-if game.PlaceId == 110258689672367 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/Doors.lua"))()
-if game.PlaceId == 6839171747 then
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "JCHUB", -- Required
-	Text = "Sorry, Currently we only support the 'Hotel-' modifier currently", -- Required
-	Icon = "rbxassetid://1234567890" -- Optional
-})
+if game.PlaceId == 12240122896 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JCdevreal/Jchub/refs/heads/main/FigureLibrary.lua"))()
